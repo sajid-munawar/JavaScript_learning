096ebe08ce91e9c700fe8f288dc7accee16cbd55
sajid507 <sajid92500@gmail.com>
2020-09-02 11:48:49 +0500

    index.js

--- Index.js
+++ Index.js
@@ -1,11 +1,9 @@
-var a=5  //6  //7  //8
-        //7   //8
-console.log(a)
-var b=++a //6 //5 //6
-console.log(a)
-var c=++a - --b - ++a + ++b
-//     7  -  5  - 8    + 6
-//    6   -  5  - 7   +  6
-console.log(a)
-console.log(b)
-console.log(c)
+var a=5;
+var b="5";
+console.log(a==b);   // true
+console.log(a===b);  // false
+console.log(a>b);    // fasle
+console.log(a<b);    // false
+console.log(a!=b);   // false
+console.log(a>=b);   // true
+console.log(a<=b);   // true
