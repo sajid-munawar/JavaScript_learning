c0592473b1ce2f7136b162aa9648008da8f034ee
sajid507 <sajid92500@gmail.com>
2020-09-04 11:25:44 +0500

    js file for find num

--- Index.js
+++ Index.js
@@ -1,9 +1,24 @@
-var a=5;
-var b="5";
-console.log(a==b);   // true
-console.log(a===b);  // false
-console.log(a>b);    // fasle
-console.log(a<b);    // false
-console.log(a!=b);   // false
-console.log(a>=b);   // true
-console.log(a<=b);   // true
+var arr=[]
+
+for (var i=0; i<11; i++){
+       arr[i]=i
+       
+    }
+var availabe=true
+var ask=parseInt(prompt("Enter number to find"))
+for (var i=0; i<arr.length; i++){
+    var b=arr[i]
+    if (b==ask){
+        console.log(ask+" is availabe in arr at "+(i+1)+" index")
+        availabe=false
+        break
+    }
+}
+if (availabe){
+    console.log("Not available")
+}
+// console.log(arr)
+    
+
+
+
