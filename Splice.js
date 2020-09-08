e2322b734fa1d0d08c4885eae0feaa326ec99e0e
sajid507 <sajid92500@gmail.com>
2020-09-08 13:43:29 +0500

    Splice

---
+++ Splice.js
@@ -0,0 +1,10 @@
+var foods=["Pizza","Burger","Snacks"]
+console.log(foods)
+foods.splice(1,0,"Sandwitch")
+console.log(foods)
+
+foods.splice(1,2,"Biryani")
+console.log(foods)
+// first element for index
+// second element for how much indexes to remove
+// Third is the element to add in array
