4f5417fbdc7e2f60dd9a34391487349d542b1f59
sajid507 <sajid92500@gmail.com>
2020-09-08 14:48:30 +0500

    Slice

---
+++ SliceInArr.js
@@ -0,0 +1,9 @@
+var arr=[]
+for(var i=0;i<11;i++ ){
+    arr[i]=i
+}
+console.log(arr)
+console.log(arr.slice( 0,3))
+console.log(arr.slice(-7,-2))
+console.log(arr.slice(-3,-8))
+console.log(arr.slice(7,2))
