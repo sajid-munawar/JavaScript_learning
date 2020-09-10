4169975074ccbcb945a361abd0b6c0cba830fc84
sajid507 <sajid92500@gmail.com>
2020-09-10 14:18:13 +0500

    Split

---
+++ Split.js
@@ -0,0 +1,17 @@
+var sentence="My name is Sajid Munawar."
+
+console.log(sentence.split(" "))
+
+var sentence="My name | is Sajid Munawar."
+
+console.log(sentence.split("|"))
+
+var sentence="My name is Sajid Munawar."
+
+console.log(sentence.split("a"))
+
+
+console.log(sentence.split(""))
+
+var b=sentence.split("")
+console.log("Length = "+b.length)
