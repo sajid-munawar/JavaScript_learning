a62ac964b908ad86f0e21111cd7034b0bbdbd382
sajid507 <sajid92500@gmail.com>
2020-09-10 12:33:02 +0500

    CharAt

---
+++ Chat_At.js
@@ -0,0 +1,10 @@
+var sentence="My name is Sajid Munawar."
+console.log(sentence)
+
+console.log("CharAt index "+4+ " = "+sentence.charAt(4))
+
+console.log("CharAt index "+6+ " = "+sentence.charAt(6))
+console.log("CharAt index "+-3+ " = "+sentence.charAt(-3))
+console.log("CharAt index "+-6+ " = "+sentence.charAt(-6))
+console.log("CharAt index "+22+ " = "+sentence.charAt(22))
+console.log("CharAt index "+44+ " = "+sentence.charAt(44))
