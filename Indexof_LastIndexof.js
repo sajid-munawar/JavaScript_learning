7e84aedbed5605de6dae84ecf5f9db6e76a69b3a
sajid507 <sajid92500@gmail.com>
2020-09-10 12:13:10 +0500

    Index of and last index of

---
+++ Indexof_LastIndexof.js
@@ -0,0 +1,17 @@
+var sentence="My name is Sajid Munawar."
+
+console.log(sentence.indexOf("Sajid"))
+
+console.log(sentence.indexOf("na"))
+
+console.log(sentence.indexOf("na",10))
+
+
+console.log(sentence.lastIndexOf("na"))
+
+console.log(sentence.lastIndexOf("na",10))
+
+var menu=["pizZA","SanDwich","BurGer","SnacKs"];
+console.log(menu.lastIndexOf("SnacKs"))
+
+console.log(menu.lastIndexOf("SanDwich"))
