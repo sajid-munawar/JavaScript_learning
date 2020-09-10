56f7985c6d1bd349b4fdae7a3852b3acfd63dae3
sajid507 <sajid92500@gmail.com>
2020-09-10 12:01:23 +0500

    Escape Characters

---
+++ Escape_Character.js
@@ -0,0 +1,14 @@
+var name="My name is \"Sajid\"."
+console.log(name)
+
+var institute="Learning from \tPIAIC."
+console.log(institute)
+
+var newLine="This is a \nNew line."
+console.log(newLine)
+
+var backSlash="This is \\ \"Backslash\""
+console.log(backSlash)
+
+var singleQuote="This is (\') \'single Quote\'. "
+console.log(singleQuote)
