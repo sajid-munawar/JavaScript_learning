fdce87229e39b9663b3dddc89cfa7340287d26a2
sajid507 <sajid92500@gmail.com>
2020-09-10 12:00:27 +0500

    String slicing

---
+++ String_Slicing.js
@@ -0,0 +1,23 @@
+var sentence="My name is Sajid Munawar."
+
+console.log("Length = "+sentence.length)
+
+console.log(sentence.slice(11,24))
+
+console.log(sentence.slice(11,17))
+
+console.log(sentence.slice(17,24))
+
+
+console.log(sentence.slice(-14,-1)) //Negetive Indexing
+
+
+console.log(sentence.slice(22,11)) //Empty string
+
+
+console.log(sentence.slice(-12,-22)) //Empty string
+
+console.log(sentence.slice(11,54))
+
+console.log(sentence.slice(-14,))
+console.log(sentence.slice( ))
