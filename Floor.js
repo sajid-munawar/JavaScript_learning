b33b598458cb6cf6ab5e9b11e9bd5e39b7236f3e
sajid507 <sajid92500@gmail.com>
2020-09-11 10:54:14 +0500

    Floor

---
+++ Floor.js
@@ -0,0 +1,14 @@
+var a=4.3
+console.log("4.3 = "+Math.floor(a))
+
+var a=4.7
+console.log("4.7 = "+Math.floor(a))
+
+var a=4.1
+console.log("4.1 = "+Math.floor(a))
+
+var a=-4.3
+console.log("-4.3 = "+Math.floor(a))
+
+var a=-4.7
+console.log("-4.7 = "+Math.floor(a))
