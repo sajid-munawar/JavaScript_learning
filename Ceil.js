be9470c03f6b59a2cf43b25b6b32e77c68e4fea1
sajid507 <sajid92500@gmail.com>
2020-09-11 10:54:03 +0500

    Ceil

---
+++ Ceil.js
@@ -0,0 +1,14 @@
+var a=4.3
+console.log("4.3 = "+Math.ceil(a))
+
+var a=4.7
+console.log("4.7 = "+Math.ceil(a))
+
+var a=4.1
+console.log("4.1 = "+Math.ceil(a))
+
+var a=-4.3
+console.log("-4.3 = "+Math.ceil(a))
+
+var a=-4.7
+console.log("-4.7 = "+Math.ceil(a))
