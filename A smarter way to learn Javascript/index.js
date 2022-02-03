46472d190192d08bf2ab7ad5235a49fe0443322a
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-03 11:52:28 +0500

    ++num

--- A smarter way to learn Javascript/index.js
+++ A smarter way to learn Javascript/index.js
@@ -3,7 +3,13 @@
 // console.log(name+'123'*2)
 // console.log("12"+"5 ")
 // console.log("12"+5 )
+
+// var num = 5 ;
+// var anum= num++ ;
+// console.log(num)
+// console.log(anum)
+
 var num = 5 ;
-var anum= num++ ;
+var anum= ++num ;
 console.log(num)
 console.log(anum)
