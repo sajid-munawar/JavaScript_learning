239633bb21a77be2d4fb08a3c5fb24667038d1f9
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-03 11:51:28 +0500

    ++ operator

--- A smarter way to learn Javascript/index.js
+++ A smarter way to learn Javascript/index.js
@@ -1,5 +1,9 @@
-// alert('Thank you for your input');
-var name = "Sajid" ;
-console.log(name+'123'*2)
-console.log("12"+"5 ")
-console.log("12"+5 )
+// // alert('Thank you for your input');
+// var name = "Sajid" ;
+// console.log(name+'123'*2)
+// console.log("12"+"5 ")
+// console.log("12"+5 )
+var num = 5 ;
+var anum= num++ ;
+console.log(num)
+console.log(anum)
