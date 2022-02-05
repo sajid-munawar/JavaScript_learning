56621e7f20f81848cc8795fabe7b8842b65745a9
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-05 12:01:21 +0500

    replace string characters

--- A smarter way to learn Javascript/index.js
+++ A smarter way to learn Javascript/index.js
@@ -69,6 +69,15 @@
 // console.log(arr.length);
 
 
-var name=prompt('Please enter your name: ');
-name = name.toUpperCase();
-console.log(name);
+// var name=prompt('Please enter your name: ');
+// name = name.toUpperCase();
+// console.log(name);
+
+var text="It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his." ;
+console.log(text)
+for (var i = 0; i < text.length; i++) {
+    if (text.slice(i, i + 12) === "World War II") {
+        text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+        }
+     }
+console.log(text)
