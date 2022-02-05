91f3016c978bb7ebd09919fbffa77a1db42f001b
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-05 21:39:29 +0500

    do while

--- A smarter way to learn Javascript/index.js
+++ A smarter way to learn Javascript/index.js
@@ -272,9 +272,17 @@
 
 // while loop
 
-var i = 0 ;
+// var i = 0 ;
 
-while (i<10){
-    console.log(i)
+// while (i<10){
+//     console.log(i)
+//     i++ ;
+// }
+
+var i = 1 ;
+
+do {
+    console.log(i);
     i++ ;
 }
+while (i<10);
