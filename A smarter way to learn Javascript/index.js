c9d49cbbda3e1a45d4b35e3c9e6e4fdce09cbbbc
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-05 12:20:03 +0500

    lastIndexOf

--- A smarter way to learn Javascript/index.js
+++ A smarter way to learn Javascript/index.js
@@ -68,8 +68,8 @@
 // name = name.toUpperCase();
 // console.log(name);
 
-var text="It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his." ;
-console.log(text)
+// var text="It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his." ;
+// console.log(text)
 // for (var i = 0; i < text.length; i++) {
 //     if (text.slice(i, i + 12) === "World War II") {
 //         text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
@@ -78,9 +78,14 @@ console.log(text)
 // console.log(text)
 
 
-var firstChar = text.indexOf("World War II");
-console.log(firstChar)
-if (firstChar !== -1) {
-    text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
-}
-console.log(text)
+// var firstChar = text.indexOf("World War II");
+// console.log(firstChar)
+// if (firstChar !== -1) {
+//     text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// }
+// console.log(text)
+
+
+var text = "To be or not to be.";
+var segIndex = text.lastIndexOf("be");
+console.log(segIndex)
