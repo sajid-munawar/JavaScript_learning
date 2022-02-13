162b0405de3ee071e0d31ff2a2687e3615ce700e
sajid-munawar <sajidmunawar666@gmail.com>
2022-02-13 18:15:44 +0500

    soloLearn ch 1

---
+++ SoloLearn/index.js
@@ -0,0 +1,17 @@
+var num = 123 ;
+var num = 'aa' ;
+console.log(num) ;
+
+
+console.log('I am a \'Programmer\'.') ;
+console.log('I am a \n\n\'Programmer\'.') ;
+
+console.log('I am a \t\t\'Programmer\'.') ;
+
+console.log('I am a \b\b\'Programmer\'.') ;
+
+console.log('I am a \f\f\'Programmer\'.') ;
+
+
+
+
