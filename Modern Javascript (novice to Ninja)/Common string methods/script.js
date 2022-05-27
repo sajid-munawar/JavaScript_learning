341d4f668a0dec4da2efad86474cd222453bc453
sajid-munawar <sajidmunawar666@gmail.com>
2022-05-27 07:54:05 +0500

    Common string methods

---
+++ Modern Javascript (novice to Ninja)/Common string methods/script.js
@@ -0,0 +1,17 @@
+let email = 'sajidmunawar666@gmail.com';
+
+// let result = email.toLocaleLowerCase();
+
+// let result = email.toUpperCase();
+
+// let result = email.lastIndexOf('m');
+
+// let result = email.slice(2,4);
+
+// let result = email.substring(3, 10);
+
+let result = email.replace('s', 'w');
+
+
+
+console.log(result);
